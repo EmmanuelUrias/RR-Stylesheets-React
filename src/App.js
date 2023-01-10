5080cf6c9d2aad580c03e4c63795a53154af0ab4
EmmanuelU <emmanuelurias60@icloud.com>
2023-01-09 20:51:37 -0800

    Made a simple timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import Clock from './Components/Clock';
+import WelcomeMsg from './Components/WelcomeMsg';
+import UserMsg from './Components/UserMsg';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [timeString, setTimeString] = useState('')
+
+      function updateTime() {
+       let currentTime = new Date();
+        let hours = currentTime.getHours().toString().padStart(2, "0");
+        let minutes = currentTime.getMinutes().toString().padStart(2, "0");
+        let seconds = currentTime.getSeconds().toString().padStart(2, "0");
+
+        // Format the time as a string
+        setTimeString(`${hours}:${minutes}:${seconds}`)          
+      }
+
+      useEffect(() => {
+        setInterval(updateTime, 1000)
+      })
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <WelcomeMsg />
+      <Clock time={timeString}/>
+      <UserMsg />
     </div>
   );
 }
---
+++ src/Components/WelcomeMsg.jsx
@@ -0,0 +1,16 @@
+
+function WelcomeMsg() {
+     const WelcomeMsgStyle = {        
+        textAlign: 'center',
+        fontSize: 'xx-large',
+        fontFamily: "'Times New Roman', Times, serif",
+        margin: '30px'
+    }
+    return(
+        <>
+        <h1 style={WelcomeMsgStyle}>Welcome to this Clock Page</h1>
+        </>
+    )
+}
+
+export default WelcomeMsg
